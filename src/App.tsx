4d2f2fac66e4a1079a2f84d3fefa996b979548e2
bobFisher1111 <rroseberry@twu.edu>
2024-02-28 22:48:02 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [apiData, setApiData] = React.useState<any>([]);
+  const fetchData = () => axios.get('https://website-api-blush.vercel.app/api/v1/students').then(
+    (response) => {
+      return response;
+    }
+  ).then((data => setApiData(data.data)))
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const data = apiData && apiData;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Vercel Proof of concept</h1>
+      {data?.map((userInfo: any) => (
+        <h1 key={userInfo.id}>{`User Name: ${userInfo.name}`}</h1>
+        ))
+      }
     </div>
   );
 }
